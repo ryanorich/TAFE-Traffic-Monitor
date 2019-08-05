98f58db7b70261f56a1a14aeb57297f8b917db6d
Ryan Rich <ryanorich@gmail.com>
2019-08-05 23:25:16 +1000

    Added Libs

---
+++ src/testing/TestingCharts.java
@@ -0,0 +1,24 @@
+package testing;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+//import de.tesis.dynaware.grapheditor.GraphEditor;
+//import de.tesis.dynaware.grapheditor.core.DefaultGraphEditor;
+
+
+
+public class TestingCharts extends Application
+{
+    static void main (String[] args)
+    {
+        launch(args);
+    }
+    @Override
+    public void start(Stage arg0) throws Exception
+    {
+
+        
+    }
+    
+}
